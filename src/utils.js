00d55b2cfc179d6f24725a6473bad6b61abab8ee
woaigmz <woaigmz@gmail.com>
2018-06-04 16:24:56 +0800

    fix utils.js -- add localStorage

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,34 @@
-exports.isEmpty = function(obj) {
+export const isEmpty = function(obj) {
     if (typeof obj == "undefined" || obj == null || obj == "") {
       return true;
     } else {
       return false;
     }
   };
+
+/**
+ * 存储localStorage
+ */
+export const setStore = (name, content) => {
+	if (!name) return;
+	if (typeof content !== 'string') {
+		content = JSON.stringify(content);
+	}
+	window.localStorage.setItem(name, content);
+}
+
+/**
+ * 获取localStorage
+ */
+export const getStore = name => {
+	if (!name) return;
+	return window.localStorage.getItem(name);
+}
+
+/**
+ * 删除localStorage
+ */
+export const removeStore = name => {
+	if (!name) return;
+	window.localStorage.removeItem(name);
+}
