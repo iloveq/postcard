329f4e3bc2290a433c24d98d4a8e7f3013318b2d
woaigmz <woaigmz@gmail.com>
2018-06-04 18:11:24 +0800

    update vuex in store

--- src/main.js
+++ src/main.js
@@ -6,14 +6,14 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import VueResource from 'vue-resource'
-import store from './store/'
+import store from './store/index'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
 Vue.use(VueResource);
 
-/* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   router,
--- src/store/action.js
+++ src/store/action.js
@@ -1,18 +1,7 @@
 import {
-	getUser,
-	getAddressList
-} from '../service/getData'
-import {
 	GET_USERINFO
 } from './mutation-types.js'
 
 export default {
 
-	async getUserInfo({
-		commit,
-		state
-	}) {
-		let res = await getUser();
-		commit(GET_USERINFO, res)
-	}
 }
