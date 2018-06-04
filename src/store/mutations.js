60bc472e7dc0ec612e6bbf12f95d0cd5c7780217
woaigmz <woaigmz@gmail.com>
2018-06-04 18:28:34 +0800

    fix mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,25 +1,19 @@
-import {
-	GET_USERINFO
-} from './mutation-types.js'
-
+import { GET_USERINFO } from './mutation-types.js'
+import {setStore,getStore, isEmpty} from '../utils'
 
 
 export default {
 	
 	//获取用户信息存入vuex
-	[GET_USERINFO](state, info) {
-		if (state.userInfo && (state.userInfo.username !== info.username)) {
-			return;
-		};
-		if (!state.login) {
-			return
-		}
-		if (!info.message) {
-			state.userInfo = {...info};
-		} else {
-			state.userInfo = null;
-		}
-	},
-	
+	[GET_USERINFO](state, userInfo) {
 
+		if(!isEmpty.userInfo){
+			state.token = userInfo.token;
+			state.isLogin = userInfo.isLogin;
+			state.imgArr = userInfo.imgArr;
+			setStore('token',state.token);
+		}
+		
+	}
+	
 }
