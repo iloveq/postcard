78ee416d78f0ee1df86390f364805adb17099c48
woaigmz <woaigmz@gmail.com>
2018-06-06 18:31:10 +0800

    card list demo

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -51,7 +51,12 @@
     </div>
     <!-- 优秀作品推荐 -->
     <div id="card-list">
-
+      <div class="card-item" v-for="(item,index) in cards" :key="index">
+        <img class="card-item-img" :src="item.imgurl">
+        <div class="card-item-userinfo">
+          {{item.name}} {{item.like}}
+        </div>
+      </div>
     </div>
     <!-- 浏览器底部toast -->
     <div id="snap" v-show="show_snap">
@@ -116,12 +121,185 @@ export default {
           name: "获赞",
           count: 0
         }
+      ],
+      cards: [
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frv032vod8j30k80q6gsz.jpg",
+          name: "︶ㄣ期待想念、、",
+          like: 1,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1fs02a9b0nvj30sg10vk4z.jpg",
+          name: "心诏不宣@",
+          like: 2,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1fryyn63fm1j30sg0yagt2.jpg",
+          name: "殇不起",
+          like: 3,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frv03m8ky5j30iz0rltfp.jpg",
+          name: " 小熊_,",
+          like: 4,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frsllc19gfj30k80tfah5.jpg",
+          name: "一别便不再见",
+          like: 5,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frslibvijrj30k80q678q.jpg",
+          name: "看看我的坏",
+          like: 6,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frrifts8l5j30j60ojq6u.jpg",
+          name: "〞将军っ",
+          like: 7,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frqscr5o00j30k80qzafc.jpg",
+          name: "伤上三五年",
+          like: 8,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frmuto5qlzj30ia0notd8.jpg",
+          name: "Super丿潮流盛典",
+          like: 9,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frjd77dt8zj30k80q2aga.jpg",
+          name: "-离沐倾城 winnie°",
+          like: 10,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frjd4var2bj30k80q0dlf.jpg",
+          name: "-离沐倾城 winnie°",
+          like: 11,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frja502w5xj30k80od410.jpg",
+          name: "-离沐倾城 winnie°",
+          like: 12,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frepr2rhxvj30qo0yjth8.jpg",
+          name: "-离沐倾城 winnie°",
+          like: 13,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frepqtwifwj30no0ti47n.jpg",
+          name: "-离沐倾城 winnie°",
+          like: 14,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frepq6mfvdj30p00wcwmq.jpg",
+          name: "-离沐倾城 winnie°",
+          like: 15,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frepozc5taj30qp0yg7aq.jpg",
+          name: "-离沐倾城 winnie°",
+          like: 16,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frevscw2wej30je0ps78h.jpg",
+          name: "-离沐倾城 winnie°",
+          like: 17,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1frg40vozfnj30ku0qwq7s.jpg",
+          name: "-离沐倾城 winnie°",
+          like: 18,
+          share: 2,
+          collect: 3
+        },
+        {
+          cardId: "",
+          imgurl:
+            "http://ww1.sinaimg.cn/large/0065oQSqly1fri9zqwzkoj30ql0w3jy0.jpg",
+          name: "-离沐倾城 winnie°",
+          like: 19,
+          share: 2,
+          collect: 3
+        }
       ]
     };
   },
   computed: mapState({
-    isLogin: 'isLogin',
-    username:'username'
+    isLogin: "isLogin",
+    username: "username"
   }),
   methods: {
     ...mapMutations(["RECORD_USERINFO"]),
@@ -210,7 +388,7 @@ export default {
                   that.showSnap("success", response.body.message);
                   //存储token
                   this.RECORD_USERINFO({
-                    username:response.body.data.username,
+                    username: response.body.data.username,
                     token: response.body.data.token,
                     isLogin: true,
                     imgArr: response.body.data.imgArr
@@ -392,14 +570,50 @@ export default {
   color: #586069;
 }
 
-#card-list{
-  
+#card-list {
+  width: 900px;
+  background: #fff;
+  margin-top: 300px;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#card-list:after {
+  content: "";
+  flex-grow: 999999999;
+}
+
+.card-item {
+  flex-grow: 1;
+  margin: 2px;
+  position: relative;
+  align-items: center;
+}
+
+.card-item-img {
+  height: 200px;
+  object-fit: cover;
+  min-width: 100%;
+  /* vertical-align: bottom; */
+}
+
+.card-item-userinfo {
+  background: white;
+  font-size: 12px;
+  font-weight: 600;
+  color: #409eff;
+  text-align: center;
+  padding: 10px;
 }
 
 #snap {
-  position: absolute;
-  bottom: 0px;
-  left: 0px;
+  left: 0;
+  position: fixed;
+  bottom: 0;
   width: 100%;
+  z-index: 100;
 }
 </style>
