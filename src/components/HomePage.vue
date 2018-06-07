35fe010cdc1fb2386c642b4832e56bbb843fe963
woaigmz <woaigmz@gmail.com>
2018-06-07 12:58:48 +0800

    fix data:fun()

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -38,7 +38,7 @@
         <i id="edit-mine-info" class="el-icon-edit"></i>
       </div>
       <!-- 尾巴 -->
-      <div class="tail-nav">
+      <div class="tail-nav" >
         <div class="nav-item" v-for="(item,index) in items" :key="index">
           <a :href="item.href" class="Item" role="tab" :title="item.name">
             {{item.name}}
@@ -50,7 +50,7 @@
       </div>
     </div>
     <!-- 优秀作品推荐 -->
-    <div id="card-list">
+    <div id="card-list" v-show="isLogin">
       <div class="card-item" v-for="(item,index) in cards" :key="index">
         <img class="card-item-img" :src="item.imgurl">
         <div class="card-item-userinfo">
@@ -75,6 +75,7 @@
 <script>
 import { isEmpty, getStore } from "../utils.js";
 import { mapState, mapMutations } from "vuex";
+import { login_form, register_form, items, cards } from "../data/localData.js";
 export default {
   name: "HomePage",
   components: {
@@ -90,216 +91,16 @@ export default {
       user_action_ctrl: "登陆",
       input_name: "",
       input_pwd: "",
-      register_form: {
-        name: "",
-        age: "",
-        sex: "",
-        address: "",
-        imgArr: "",
-        phone: "",
-        password: "",
-        token: ""
-      },
-      login_form: {
-        name: "",
-        password: "",
-        token: ""
-      },
-      items: [
-        {
-          href: "",
-          name: "粉丝",
-          count: 0
-        },
-        {
-          href: "",
-          name: "关注",
-          count: 0
-        },
-        {
-          href: "",
-          name: "获赞",
-          count: 0
-        }
-      ],
-      cards: [
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frv032vod8j30k80q6gsz.jpg",
-          name: "︶ㄣ期待想念、、",
-          like: 1,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1fs02a9b0nvj30sg10vk4z.jpg",
-          name: "心诏不宣@",
-          like: 2,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1fryyn63fm1j30sg0yagt2.jpg",
-          name: "殇不起",
-          like: 3,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frv03m8ky5j30iz0rltfp.jpg",
-          name: " 小熊_,",
-          like: 4,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frsllc19gfj30k80tfah5.jpg",
-          name: "一别便不再见",
-          like: 5,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frslibvijrj30k80q678q.jpg",
-          name: "看看我的坏",
-          like: 6,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frrifts8l5j30j60ojq6u.jpg",
-          name: "〞将军っ",
-          like: 7,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frqscr5o00j30k80qzafc.jpg",
-          name: "伤上三五年",
-          like: 8,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frmuto5qlzj30ia0notd8.jpg",
-          name: "Super丿潮流盛典",
-          like: 9,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frjd77dt8zj30k80q2aga.jpg",
-          name: "-离沐倾城 winnie°",
-          like: 10,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frjd4var2bj30k80q0dlf.jpg",
-          name: "-离沐倾城 winnie°",
-          like: 11,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frja502w5xj30k80od410.jpg",
-          name: "-离沐倾城 winnie°",
-          like: 12,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frepr2rhxvj30qo0yjth8.jpg",
-          name: "-离沐倾城 winnie°",
-          like: 13,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frepqtwifwj30no0ti47n.jpg",
-          name: "-离沐倾城 winnie°",
-          like: 14,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frepq6mfvdj30p00wcwmq.jpg",
-          name: "-离沐倾城 winnie°",
-          like: 15,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frepozc5taj30qp0yg7aq.jpg",
-          name: "-离沐倾城 winnie°",
-          like: 16,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frevscw2wej30je0ps78h.jpg",
-          name: "-离沐倾城 winnie°",
-          like: 17,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1frg40vozfnj30ku0qwq7s.jpg",
-          name: "-离沐倾城 winnie°",
-          like: 18,
-          share: 2,
-          collect: 3
-        },
-        {
-          cardId: "",
-          imgurl:
-            "http://ww1.sinaimg.cn/large/0065oQSqly1fri9zqwzkoj30ql0w3jy0.jpg",
-          name: "-离沐倾城 winnie°",
-          like: 19,
-          share: 2,
-          collect: 3
-        }
-      ]
+      register_form,
+      login_form,
+      items,
+      cards
     };
   },
   computed: mapState({
     isLogin: "isLogin",
-    username: "username"
+    username: "username",
+    token: "token"
   }),
   methods: {
     ...mapMutations(["RECORD_USERINFO"]),
@@ -373,8 +174,7 @@ export default {
       var that = this;
       this.login_form.name = this.input_name;
       this.login_form.password = this.input_pwd;
-      // token 从 vuex 获取
-      this.login_form.token = "";
+      this.login_form.token = this.token;
       if (isEmpty(this.input_name) || isEmpty(this.input_pwd)) {
         that.showSnap("error", "输入格式错误");
         console.log("112");
@@ -573,7 +373,7 @@ export default {
 #card-list {
   width: 900px;
   background: #fff;
-  margin-top: 300px;
+  margin-top: 30px;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
   margin-left: auto;
   margin-right: auto;
---
+++ src/data/localData.js
@@ -0,0 +1,209 @@
+exports.login_form = {
+  name: "",
+  password: "",
+  token: ""
+};
+
+exports.register_form ={
+  name: "",
+  age: "",
+  sex: "",
+  address: "",
+  imgArr: "",
+  phone: "",
+  password: "",
+  token: ""
+};
+
+exports.items = [
+  {
+    href: "",
+    name: "粉丝",
+    count: 0
+  },
+  {
+    href: "",
+    name: "关注",
+    count: 0
+  },
+  {
+    href: "",
+    name: "获赞",
+    count: 0
+  }
+];
+
+exports.cards = [
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frv032vod8j30k80q6gsz.jpg",
+      name: "︶ㄣ期待想念、、",
+      like: 1,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1fs02a9b0nvj30sg10vk4z.jpg",
+      name: "心诏不宣@",
+      like: 2,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1fryyn63fm1j30sg0yagt2.jpg",
+      name: "殇不起",
+      like: 3,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frv03m8ky5j30iz0rltfp.jpg",
+      name: " 小熊_,",
+      like: 4,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frsllc19gfj30k80tfah5.jpg",
+      name: "一别便不再见",
+      like: 5,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frslibvijrj30k80q678q.jpg",
+      name: "看看我的坏",
+      like: 6,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frrifts8l5j30j60ojq6u.jpg",
+      name: "〞将军っ",
+      like: 7,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frqscr5o00j30k80qzafc.jpg",
+      name: "伤上三五年",
+      like: 8,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frmuto5qlzj30ia0notd8.jpg",
+      name: "Super丿潮流盛典",
+      like: 9,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frjd77dt8zj30k80q2aga.jpg",
+      name: "-离沐倾城 winnie°",
+      like: 10,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frjd4var2bj30k80q0dlf.jpg",
+      name: "-离沐倾城 winnie°",
+      like: 11,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frja502w5xj30k80od410.jpg",
+      name: "-离沐倾城 winnie°",
+      like: 12,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frepr2rhxvj30qo0yjth8.jpg",
+      name: "-离沐倾城 winnie°",
+      like: 13,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frepqtwifwj30no0ti47n.jpg",
+      name: "-离沐倾城 winnie°",
+      like: 14,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frepq6mfvdj30p00wcwmq.jpg",
+      name: "-离沐倾城 winnie°",
+      like: 15,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frepozc5taj30qp0yg7aq.jpg",
+      name: "-离沐倾城 winnie°",
+      like: 16,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frevscw2wej30je0ps78h.jpg",
+      name: "-离沐倾城 winnie°",
+      like: 17,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1frg40vozfnj30ku0qwq7s.jpg",
+      name: "-离沐倾城 winnie°",
+      like: 18,
+      share: 2,
+      collect: 3
+    },
+    {
+      cardId: "",
+      imgurl:
+        "http://ww1.sinaimg.cn/large/0065oQSqly1fri9zqwzkoj30ql0w3jy0.jpg",
+      name: "-离沐倾城 winnie°",
+      like: 19,
+      share: 2,
+      collect: 3
+    }
+  ];
+
