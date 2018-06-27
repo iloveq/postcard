b2139ab9928a5ebf04b239664223d6bcf5e276f8
woaigmz <woaigmz@gmail.com>
2018-06-27 14:49:05 +0800

    update main.js the turn for element-ui css style

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 /* jshint esversion: 6 */ 
 import Vue from 'vue';
-import App from './App';
-import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import App from './App';
+import router from './router';
 import VueResource from 'vue-resource';
 import store from './store/index';
 
